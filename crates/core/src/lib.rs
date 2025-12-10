5e9ae6e2f7ba1e9315dd0f0a54fc7fb087bb07ba
lumoswiz <lumoswiz@protonmail.com>
2025-12-10 17:50:23 +0100

    feat(core): add block producer skeleton for block streaming

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,11 +1,13 @@
+pub mod blocks;
+pub mod client;
 pub mod error;
 pub mod hooks;
-pub mod client;
 pub mod types;
 pub mod validation;
 
+pub use blocks::*;
+pub use client::*;
 pub use error::*;
 pub use hooks::*;
-pub use client::*;
 pub use types::*;
 pub use validation::*;
